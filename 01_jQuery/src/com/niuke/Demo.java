5cb349b3cdcf34631dcbb0700612250921e1b6e9
wangruqian <1069867306@qq.com>
2018-12-09 16:40:12 +0800

    我的第一个eclipse项目

---
+++ 01_jQuery/src/com/niuke/Demo.java
@@ -0,0 +1,26 @@
+package com.niuke;
+
+public class Demo {
+	class Super{
+		int flag=1;
+		public Super() {
+			test();
+		}
+		public void test() {
+			System.out.println("Super.test() flag="+flag);
+		}
+	}
+	class Sub extends Super{
+		public Sub(int i) {
+			flag=i;
+			System.out.println("Sub.Sub() flag="+flag);
+		}
+		public void test() {
+			System.out.println("Sub.test() flag="+flag);
+		}
+	}
+	public static void main(String[] args) {
+		new Demo().new Sub(5);
+		}
+
+}
